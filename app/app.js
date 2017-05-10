5c0cab800c15824b773bdc784f2d762c644649cb
mungaiandela <martin.mungai@andela.com>
2017-05-10 11:36:47 +0300

    [Feature] App.js file declaring angular module

---
+++ app/app.js
@@ -0,0 +1,16 @@
+// Controllers, directives and modules
+
+'use strict';
+
+var app = angular.module('bucketlist', [
+  'ngRoute',
+  'bucketlist.landing',
+  'bucketlist.signin',
+  'bucketlist.signup',
+  'bucketlist.home',
+  'bucketlist.view'
+  ]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .otherwise({redirectTo: '/view'});
+}]);
