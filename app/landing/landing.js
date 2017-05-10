c23d94cdc2d6cf6b130b932227a150d3c0f042c5
mungaiandela <martin.mungai@andela.com>
2017-05-10 11:38:47 +0300

    [Feature] Landing folder with js controller and markup for the landing page

---
+++ app/landing/landing.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('bucketlist.landing', ['ngRoute'])
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/landing', {
+        templateUrl: 'landing/landing.html', 
+        controller: 'LandingCtrl'
+    });
+}])
+
+app.controller('LandingCtrl', [function(){
+    
+}]);
