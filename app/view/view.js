0b70764efcac4d63eaa505186ad5d3dc92e82858
mungaiandela <martin.mungai@andela.com>
2017-05-11 08:35:53 +0300

    [Feature] Add Delete, Edit and Search functionality

--- app/view/view.js
+++ app/view/view.js
@@ -14,26 +14,77 @@ app.controller('ViewCtrl', function($scope, $http, $location, $window){
    $http.get("http://127.0.0.1:5000/v1/bucketlists/", 
    {
        headers: {'Authorization': token}
-   }
-   ).then(function(response){
-       console.log(response.data)
+   }).then(function(response){
        $scope.bucketlist = response.data["bucketlists"]
        $scope.next_url = response.data.next_url
-       var next_url = response.data.next_url
-       console.log(next_url)
-       
+       $scope.prev_url = response.data.prev_url   
+    }, function(response){
+        var status = response.status
+        if(status != 200){
+            $location.path('/home');
+       }
     });
 
     $scope.getdata = function(next_url){
         $http.get(String(next_url),
        {
            headers: { 'Authorization': token}
-       }
-       ).then(function(response){
+       }).then(function(response){
+            $scope.bucketlist = response.data["bucketlists"]
+            $scope.next_url = response.data.next_url
+            $scope.prev_url = response.data.prev_url 
+            
+       },       
+    function(response){
+        var status = response.status
+            if(status != 200){
+                $scope.msg = "No more bucketlists"
+            }
+   });
+};
+
+    $scope.searchdata = function(searchparams){
+        $http.get("http://127.0.0.1:5000/v1/bucketlists/?q="+String(searchparams),
+        {
+             headers: { 'Authorization': token}
+        }).then(function(response){
             $scope.bucketlist = response.data["bucketlists"]
             $scope.next_url = response.data.next_url
             $scope.prev_url = response.data.prev_url
-           
-   });
-   };
+            $scope.msg = response.data.message
+            $location.path('/view');
+        },
+        function(response){
+            if(status != 200){
+                $scope.msg = "Bucketlist does not exist."
+            }
+        }
+        );
+    }
+
+    $scope.deletedata = function(params){
+        $http.delete("http://127.0.0.1:5000/v1/bucketlists/"+ params,
+        {
+            headers: { 'Authorization': token}
+        }).then(function(response){
+            $scope.msg = "Deleted successfully"
+            $location.path('/view');
+        });
+    }
+
+    $scope.editdata = function(title, id){
+        var data = {
+            title: title
+        };
+        $http.put("http://127.0.0.1:5000/v1/bucketlists/"+ id, JSON.stringify(data),
+        {
+             headers: { 'Authorization': token}
+
+        }).then(function(response){
+            $scope.msg = response.data.message
+
+        })
+    }
+
+    $
 });
