e865df183cfff3a137ec63d74a7b9bb2ed38d117
mungaiandela <martin.mungai@andela.com>
2017-05-11 18:40:10 +0300

    [Feature] Add create logic for create view

--- app/home/home.js
+++ app/home/home.js
@@ -2,40 +2,37 @@
 
 var app = angular.module('bucketlist.home', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: 'home/home.html',
     controller: 'HomeCtrl'
   });
 }])
 
-app.controller('HomeCtrl', function($scope, $http, $location, $window){
-  $scope.email=null;
-  $scope.password=null;
-  $scope.postdata = function(title){
+app.controller('HomeCtrl', function ($scope, $http, $location, $window) {
+  $scope.email = null;
+  $scope.password = null;
+  $scope.postdata = function (title) {
     var data = {
       title: title
     };
     var token = $window.localStorage.getItem("Authorization");
-    console.log(token);
 
-
-  $http.post("http://127.0.0.1:5000/v1/bucketlists/", JSON.stringify(data),
-    {
-        headers: {'Authorization': token}
-    }
-   ).then(function(response){
-    if (response.data){
-    $scope.msg = response.data.message;
-    $location.path('/home');
-    }
-  },
-  function(response){
-    $scope.statusval=response.status;
-    $scope.statustext = response.message;
-    
-  }
-
-  )};
+    $http.post("http://127.0.0.1:5000/v1/bucketlists/", JSON.stringify(data),
+      {
+        headers: { 'Authorization': token }
+      }
+    ).then(function (response) {
+      if (response.data) {
+        $scope.msg = response.data.message;
+        $location.path('/home');
+      }
+    },
+      function (response) {
+        $scope.statusval = response.status;
+        $scope.statustext = response.message;
+      }
+      )
+  };
 
 });
