0deada2020e435a3965f614dcd8f1f20d4207763
mungaiandela <martin.mungai@andela.com>
2017-05-11 19:01:39 +0300

    [Feature] Add signup controller

--- app/signup/signup.js
+++ app/signup/signup.js
@@ -2,20 +2,20 @@
 
 var app = angular.module('bucketlist.signup', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/signup', {
-    templateUrl: 'signup/signup.html',
-    controller: 'SignUpCtrl'
-  });
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/signup', {
+        templateUrl: 'signup/signup.html',
+        controller: 'SignUpCtrl'
+    });
 }])
 
-app.controller('SignUpCtrl', function($scope, $http, $location) {
-    $scope.first_name=null;
-    $scope.last_name=null;
-    $scope.email=null;
-    $scope.password=null;
-    $scope.confirm_password=null;
-    $scope.postdata = function(first_name, last_name, email, password, confirm_password){
+app.controller('SignUpCtrl', function ($scope, $http, $location) {
+    $scope.first_name = null;
+    $scope.last_name = null;
+    $scope.email = null;
+    $scope.password = null;
+    $scope.confirm_password = null;
+    $scope.postdata = function (first_name, last_name, email, password, confirm_password) {
         var data = {
             first_name: first_name,
             last_name: last_name,
@@ -23,31 +23,31 @@ app.controller('SignUpCtrl', function($scope, $http, $location) {
             password: password,
             confirm_password: confirm_password
         };
-    $http.post("http://127.0.0.1:5000/v1/auth/register", JSON.stringify(data)).then(function(response){
-        if (response.data){
-            $scope.msg=response.message;
-            var data = {
-                email: $scope.email,
-                password: $scope.password
-                
-            };
-            $http.post("http://127.0.0.1:5000/v1/auth/login", JSON.stringify(data)).then(function(response){
-            if (response.data){
+        $http.post("http://127.0.0.1:5000/v1/auth/register", JSON.stringify(data)).then(function (response) {
+            if (response.data) {
                 $scope.msg = response.message;
-                $location.path('/landing');
+                var data = {
+                    email: $scope.email,
+                    password: $scope.password
+
+                };
+                $http.post("http://127.0.0.1:5000/v1/auth/login", JSON.stringify(data)).then(function (response) {
+                    if (response.data) {
+                        $scope.msg = response.message;
+                        $location.path('/landing');
+                    }
+
+                });
+
             }
-            
-        });
-            
-        }
-    },
-    function(response){
-        $scope.statusval = response.status;
-        $scope.statustext = response.message;
-     
-    });
+        },
+            function (response) {
+                $scope.statusval = response.status;
+                $scope.statustext = response.message;
+
+            });
     };
-    
-   
-        
+
+
+
 });
