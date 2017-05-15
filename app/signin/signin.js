1c39bc0c0ee0be83d9516e4c9fcb215e51bd99dc
mungaiandela <martin.mungai@andela.com>
2017-05-15 18:05:17 +0300

    [Feature] Use auth service to login

--- app/signin/signin.js
+++ app/signin/signin.js
@@ -2,34 +2,28 @@
 
 var app = angular.module('bucketlist.signin', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/signin', {
-    templateUrl: 'signin/signin.html',
-    controller: 'SignInCtrl'
-  });
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/signin', {
+        templateUrl: 'signin/signin.html',
+        controller: 'SignInCtrl'
+    });
 }])
 
-app.controller('SignInCtrl', function($scope, $http, $location, $window){
-  $scope.email=null;
-  $scope.password=null;
-  $scope.postdata = function(email, password){
-    var data = {
-      email: email,
-      password: password
+app.controller('SignInCtrl', function ($scope, auth_api, $location, $window) {
+    $scope.postdata = function (email, password) {
+        auth_api.loginUser(email, password).then(function (response) {
+            if (response) {
+                $scope.msg = response.data.message;
+                $window.localStorage.setItem('Authorization', response.data.auth_token)
+                $location.path('/home');
+            }
+        },
+            function (response) {
+                $scope.statusval = response.status;
+                $scope.statustext = response.message;
+            }
+
+        )
     };
-
-  $http.post("http://127.0.0.1:5000/v1/auth/login", JSON.stringify(data)).then(function(response){
-    if (response){
-    $scope.msg = response.data.message;
-    $window.localStorage.setItem('Authorization', response.data.auth_token)
-    $location.path('/home');
-    }
-  },
-  function(response){
-    $scope.statusval=response.status;
-    $scope.statustext = response.message;
-  }
-
-  )};
 
 });
