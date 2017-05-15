a99efcf673447fc670ff1cec2a9fa916291c7fa2
mungaiandela <martin.mungai@andela.com>
2017-05-15 18:00:32 +0300

    [Chore] Refactor code formatting

--- app/app.js
+++ app/app.js
@@ -3,23 +3,23 @@
 'use strict';
 
 var app = angular.module('bucketlist', [
-  'ngRoute',
-  'bucketlist.landing',
-  'bucketlist.signin',
-  'bucketlist.signup',
-  'bucketlist.home',
-  'bucketlist.view',
-  'bucketlist.items'
+    'ngRoute',
+    'bucketlist.landing',
+    'bucketlist.signin',
+    'bucketlist.signup',
+    'bucketlist.home',
+    'bucketlist.view',
+    'bucketlist.items'
 ]).
-  config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .otherwise({ redirectTo: '/view' });
-  }]);
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .otherwise({ redirectTo: '/view' });
+    }]);
 
-app.controller('LogoutCtrl', function($scope, $location, $window){
-  $scope.logout = function(){
-    $window.localStorage.removeItem('Authorization');
-    $location.path('/signin');
-  };
+app.controller('LogoutCtrl', function ($scope, $location, $window) {
+    $scope.logout = function () {
+        $window.localStorage.removeItem('Authorization');
+        $location.path('/signin');
+    };
 })
 
