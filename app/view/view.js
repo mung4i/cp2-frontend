a8cc4461496895d08e49f816347620bca3d607ce
mungaiandela <martin.mungai@andela.com>
2017-05-15 17:00:29 +0300

    [Feature] Refactor code to use services defined in bucketlist.js

--- app/view/view.js
+++ app/view/view.js
@@ -11,98 +11,67 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 
 
-app.controller('ViewCtrl', function ($scope, $http, $location, $window) {
+app.controller('ViewCtrl', function ($scope, bucketlist_api, $http, $location, $window, $route) {
     var token = $window.localStorage.getItem("Authorization");
 
-    $http.get("http://127.0.0.1:5000/v1/bucketlists/",
-        {
-            headers: { 'Authorization': token }
-        }).then(function (response) {
+    bucketlist_api.getBucketlists().then(function (response) {
+        $scope.bucketlist = response.data["bucketlists"]
+        $scope.next_url = response.data.next_url
+        $scope.prev_url = response.data.prev_url
+    }, function (response) {
+        var status = response.status
+        if (status == 401) {
+            $location.path('/signin');
+        }
+        else {
+            $location.path('/home');
+        }
+
+    });
+
+    $scope.getdata = function (url) {
+        bucketlist_api.getNextBucketlist(url).then(function (response) {
             $scope.bucketlist = response.data["bucketlists"]
             $scope.next_url = response.data.next_url
             $scope.prev_url = response.data.prev_url
-        }, function (response) {
-            var status = response.status
-            if (status == 401) {
-                $location.path('/signin');
-            }
-            else {
-                $location.path('/home');
-            }
 
-        });
-
-    $scope.getdata = function (next_url) {
-        $http.get(String(next_url),
-            {
-                headers: { 'Authorization': token }
-            }).then(function (response) {
-                $scope.bucketlist = response.data["bucketlists"]
-                $scope.next_url = response.data.next_url
-                $scope.prev_url = response.data.prev_url
-
-            },
+        },
             function (response) {
                 var status = response.status
                 if (status != 200) {
                     $scope.msg = "No more bucketlists"
                 }
             });
-    };
+    }
 
     $scope.searchdata = function (searchparams) {
-        $http.get("http://127.0.0.1:5000/v1/bucketlists/?q=" + String(searchparams),
-            {
-                headers: { 'Authorization': token }
-            }).then(function (response) {
-                $scope.bucketlist = response.data["bucketlists"]
-                $scope.next_url = response.data.next_url
-                $scope.prev_url = response.data.prev_url
-                $scope.msg = response.data.message
-                $location.path('/view');
-            },
+        bucketlist_api.searchBucketlists(searchparams).then(function (response) {
+            $scope.bucketlist = response.data["bucketlists"]
+            $scope.next_url = response.data.next_url
+            $scope.prev_url = response.data.prev_url
+            $scope.msg = response.data.message
+            $route.reload();
+        },
             function (response) {
                 if (status != 200) {
                     $scope.msg = "Bucketlist does not exist."
                 }
             }
-            );
+        );
     }
 
     $scope.deletedata = function (params) {
-        $http.delete("http://127.0.0.1:5000/v1/bucketlists/" + params,
-            {
-                headers: { 'Authorization': token }
-            }).then(function (response) {
-                $scope.msg = "Deleted successfully"
-                $location.path('/view');
-            });
+        bucketlist_api.deleteBucketlist(params).then(function (response) {
+            $route.reload();
+            $scope.msg = "Deleted successfully"
+            
+        });
     }
 
     $scope.editdata = function (title, id) {
-        var data = {
-            title: title
-        };
-        $http.put("http://127.0.0.1:5000/v1/bucketlists/" + id, JSON.stringify(data),
-            {
-                headers: { 'Authorization': token }
+        bucketlist_api.editBucketlist(title, id).then(function (response) {
+            $scope.msg = response.data.message
 
-            }).then(function (response) {
-                $scope.msg = response.data.message
-
-            })
-    }
-
-    $scope.postitem = function (name, id) {
-        var data = {
-            name: name
-        };
-        $http.post("http://127.0.0.1:5000/v1/bucketlists/" + id + "/items/", JSON.stringify(data),
-            {
-                headers: { 'Authorization': token }
-            }).then(function (response) {
-                $scope.msg = response.data.message
-                $route.reload();
-            })
+        });
     }
 });
